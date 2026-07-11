826078068e64e3f4f5f5bbfcbea3328fad59a7e3
nikitawani86 <nikitawani83@gmail.com>
2026-07-11 16:38:58 +0530

    Adding the update payment status api

--- payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +61,7 @@ public class PaymentController {
 	}
 
 	// API for updating the Payment Status
+	@PutMapping("{paymentReference}")
 	public ResponseEntity<ApiResponse<PaymentResponse>> updatePaymentStatus(@PathVariable UUID paymentReference , 
 			@Valid @RequestBody UpdatePaymentStatusRequest request){
 		
