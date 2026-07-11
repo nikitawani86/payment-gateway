cfe69f04d609bc246c79be200756a496f3365c94
nikitawani86 <nikitawani83@gmail.com>
2026-07-12 00:24:00 +0530

    Added changes in update merchant api

--- merchant-service/src/main/java/com/example/merchant_service/merchant_service/controller/MerchantController.java
+++ merchant-service/src/main/java/com/example/merchant_service/merchant_service/controller/MerchantController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +64,7 @@ public class MerchantController {
 	}
 	
 	//Update the Merchant
+	@PutMapping("{merchantReference}")
 	public ResponseEntity<ApiResponse<CreateMerchantResponse>> updateMerchant(@PathVariable UUID merchantReference, @Valid @RequestBody UpdateMerchantRequest request ){
 		
 		CreateMerchantResponse response = merchantService.updateMerchant(request, merchantReference);
